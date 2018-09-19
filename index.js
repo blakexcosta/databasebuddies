02893aa7a003e834130f57e7aed5620f804ad0f6
bac5899 <bac5899@g.rit.edu>
2018-09-19 15:31:09 -0400

    form example

--- index.js
+++ index.js
@@ -1,89 +1,31 @@
-var express = require("express");
-var app = express();
+var express = require('express');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
+var multer = require('multer');
+var upload = multer();
+var app = express();
 
+app.get('/', function(req, res){
+   res.render('form');
+});
 
-//better separation of concerns
-//uses app.use to 'use' the module things.js
-var things = require('./things.js');
-app.use('/things', things);
-//parse url encoded data
-app.use(bodyParser.urlencoded({extended: false}));
-//parse json data
-app.use(bodyParser.json());
-//parses cookie header abd populates req.cookies w/ object keyed by cookie names
-app.use(cookieParser());
-//pug is different from the others above, with pug (which is a templating engine)
-//we don't need to 'require' it. just use it like below (in your index.js)
 app.set('view engine', 'pug');
-app.set('views','./views');
-//using multiple folders to server static files
+app.set('views', './views');
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-form-urlencoded header requests. w
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+//parsing form data/multipart data
+app.use(upload.array());
 app.use(express.static('public'));
-app.use(express.static('images'));
-//can also do it this way
-//app.use('/static', express.static('public')); // basically reassigns ability to reference code in template folders, would use /static/main.js instead of /public/main.js (it's aliasing basically)
 
-//static usage page for 'dalord'
-app.get('/dalord',function(req, res){
-	res.render('dalord');
-}); 
-
-
-//I added a /views folder and created a new pug file. look at the format
-//now I'm loading the page. all I gotta do is give it the name of the template
-// (because /views folder is alread loaded when i did app.set above)
-app.get('/first_template', function(req, res){
-	res.render('first_view');
-});
-
-
-//passing values to a template
-app.get('/login', function(req, res, next){
-        res.render("login", {user: {name: "Bobs Burgers", age:"46"}});
-});
-
- 	
-app.get('/components', function(req, res, next){
-	res.render("content");
-});
-
-
-
-
-//dynamic routes
-//complex example is in things.js
-//allows us to use parameters such as req.params.id
-//:id should show up as some number ex:) 123
-//    http://localhost:3000/123 
-app.get('/:id', function(req, res){
-	res.send('The id you specified is ' + req.params.id);
-});
-
-
-
-
-//First middleware before response is sent
-app.use(function(req, res, next){
-   console.log("Start");
-   next();
-});
-//Route handler, middleware above deligates to this router when going to localhost:3000
-app.get('/', function(req, res, next){
-   res.send("Middle");
-   next();
-});
-app.use('/', function(req, res){
-   console.log('End');
-});
-
-
-//for routes that do not match, equivalent to 404
-//should be placed AFTER all other routes, including external routers required
-app.get('*', function(req, res){
-	res.send('sorry, invalid url.');
+app.post('/',function(req,res){
+	console.log(req.body); // printing out the request body
+	res.send('receive request');
 });
 
 
 app.listen(3000);
-

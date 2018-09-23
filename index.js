97c0156e483e6c341bae9aa42089b18460783978
bac5899 <bac5899@g.rit.edu>
2018-09-23 13:44:01 -0400

    added in ability to poll the open beer api, as well as adding new request package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
+var request = require('request');
 var app = express();
 
 app.get('/', function(req, res){
@@ -27,5 +28,13 @@ app.post('/',function(req,res){
 	res.send('receive request');
 });
 
-
+//making api request call, uncomment to see
+/*request('https://data.opendatasoft.com/api/records/1.0/search/?dataset=open-beer-database%40public-us&rows=0&facet=style_name&facet=cat_name&facet=name_breweries&facet=country',  function(error,response, body) {
+  if (!error && response.statusCode == 200) {
+	//body contains all of information
+    console.log(body) // Print the google web page.
+	
+  }
+});
+*/
 app.listen(3000);

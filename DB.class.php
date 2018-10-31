3de8a93f644b0e7236bbe9adc8f74dcca5b71ac0
Matthew Turczmanovicz <MattTurcz@gmail.com>
2018-10-30 20:43:27 -0400

    The project has been migrated to use PostgreSQL. There is a script in database > PostgreSQL to load up the DB. Then the DB.class.php file has also been modified to handle PostgreSQL.

--- DB.class.php
+++ DB.class.php
@@ -11,7 +11,7 @@
             try
             {
                 //May have to change this line to connect to your MySQL
-                $this->dbh = new PDO('mysql:host=127.0.0.1;dbname=BeerBuddies_DB', 'root', 'HerroBob!');
+                $this->dbh = new PDO('pgsql:host=127.0.0.1;port=5432;dbname=beerbuddies_db', 'postgres', 'student');
                 $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				$this->dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             }
@@ -27,18 +27,16 @@
             try
             {
                 $someBeers = array();
-                $stmt = $this->dbh->prepare("select Beer.INTBEERID as BeerID,
-                (select BeerName.VCHBEERNAME from BeerName where Beer.INTBEERID = BeerName.INTBEERID) as BeerName,
-                (select Category.VCHCATEGORY from Category where Beer.INTCATEGORYID = Category.INTCATEGORYID) as categoryName,
-                (select Style.VCHSTYLE from Style where Beer.INTSTYLEID = Style.INTSTYLEID) as styleName,
-                (select Brewer.VCHBREWER from Brewer where Beer.INTBREWERID = Brewer.INTBREWERID) as brewerName,
-                Beer.VCHADDRESS as address, Beer.VCHCITY as city, Beer.VCHSTATE as state, Beer.VCHCOUNTRY as country,
-                Beer.VCHDESCRIPTION as description, Beer.VCHWEBSITE as website, Beer.INTINTERNATIONALBITTERNESSUT as intBitternessUt,
-                Beer.INTSTANDARDREFMETH as standRefMeth, Beer.INTUNVPRODCODE as uniProdCode, Beer.DATELASTUPDATED as lastUpdate,
-                Beer.VCHCOORDINATES as coords, Beer.DATEADDED as dateAdded
-                from Beer limit 10;");
-								
-								//$stmt = $this->dbh->prepare("select * from Beer;");
+                $stmt = $this->dbh->prepare('select Beer."INTBEERID" as BeerID,
+                (select BeerName."VCHBEERNAME" from beerbuddies_db.BeerName where Beer."INTBEERID" = BeerName."INTBEERID") as BeerName,
+                (select Category."VCHCATEGORY" from beerbuddies_db.Category where Beer."INTCATEGORYID" = Category."INTCATEGORYID") as categoryName,
+                (select Style."VCHSTYLE" from beerbuddies_db.Style where Beer."INTSTYLEID" = Style."INTSTYLEID") as styleName,
+                (select Brewer."VCHBREWER" from beerbuddies_db.Brewer where Beer."INTBREWERID" = Brewer."INTBREWERID") as brewerName,
+                Beer."VCHADDRESS" as address, Beer."VCHCITY" as city, Beer."VCHSTATE" as state, Beer."VCHCOUNTRY" as country,
+                Beer."VCHDESCRIPTION" as description, Beer."VCHWEBSITE" as website, Beer."INTINTERNATIONALBITTERNESSUT" as intBitternessUt,
+                Beer."INTSTANDARDREFMETH" as standRefMeth, Beer."INTUNVPRODCODE" as uniProdCode, Beer."DATELASTUPDATED" as lastUpdate,
+                Beer."VCHCOORDINATES" as coords, Beer."DATEADDED" as dateAdded
+                from beerbuddies_db.Beer limit 10;');
                 
                 $stmt->setFetchMode(PDO::FETCH_CLASS, "Beer");
                 $stmt->execute();
@@ -48,7 +46,7 @@
             }
             catch (PDOException $pdoex)
             {
-                echo "<h2>Unable to create your account.</h2>";
+                echo "<h2>Unable to retrieve the beers!</h2>";
 								echo $pdoex;
             }
         }
